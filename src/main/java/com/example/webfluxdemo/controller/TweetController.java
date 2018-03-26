32703cb21bc21e29f39b33120c56ade240b173bb
Francisco Garrido Mosqueira <garridof@ryanair.com>
2018-03-26 14:23:24 +0200

    Inital error commit

--- src/main/java/com/example/webfluxdemo/controller/TweetController.java
+++ src/main/java/com/example/webfluxdemo/controller/TweetController.java
@@ -31,41 +31,41 @@ public class TweetController {
         return tweetRepository.save(tweet);
     }
 
-    @GetMapping("/tweets/{id}")
-    public Mono<ResponseEntity<Tweet>> getTweetById(@PathVariable(value = "id") String tweetId) {
-        return tweetRepository.findById(tweetId)
-                .map(savedTweet -> ResponseEntity.ok(savedTweet))
-                .defaultIfEmpty(ResponseEntity.notFound().build());
-    }
-
-    @PutMapping("/tweets/{id}")
-    public Mono<ResponseEntity<Tweet>> updateTweet(@PathVariable(value = "id") String tweetId,
-                                                   @Valid @RequestBody Tweet tweet) {
-        return tweetRepository.findById(tweetId)
-                .flatMap(existingTweet -> {
-                    existingTweet.setText(tweet.getText());
-                    return tweetRepository.save(existingTweet);
-                })
-                .map(updateTweet -> new ResponseEntity<>(updateTweet, HttpStatus.OK))
-                .defaultIfEmpty(new ResponseEntity<>(HttpStatus.NOT_FOUND));
-    }
-
-    @DeleteMapping("/tweets/{id}")
-    public Mono<ResponseEntity<Void>> deleteTweet(@PathVariable(value = "id") String tweetId) {
-
-        return tweetRepository.findById(tweetId)
-                .flatMap(existingTweet ->
-                        tweetRepository.delete(existingTweet)
-                            .then(Mono.just(new ResponseEntity<Void>(HttpStatus.OK)))
-                )
-                .defaultIfEmpty(new ResponseEntity<>(HttpStatus.NOT_FOUND));
-    }
-
-    // Tweets are Sent to the client as Server Sent Events
-    @GetMapping(value = "/stream/tweets", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
-    public Flux<Tweet> streamAllTweets() {
-        return tweetRepository.findAll();
-    }
+//    @GetMapping("/tweets/{id}")
+//    public Mono<ResponseEntity<Tweet>> getTweetById(@PathVariable(value = "id") String tweetId) {
+//        return tweetRepository.findById(tweetId)
+//                .map(savedTweet -> ResponseEntity.ok(savedTweet))
+//                .defaultIfEmpty(ResponseEntity.notFound().build());
+//    }
+//
+//    @PutMapping("/tweets/{id}")
+//    public Mono<ResponseEntity<Tweet>> updateTweet(@PathVariable(value = "id") String tweetId,
+//                                                   @Valid @RequestBody Tweet tweet) {
+//        return tweetRepository.findById(tweetId)
+//                .flatMap(existingTweet -> {
+//                    existingTweet.setText(tweet.getText());
+//                    return tweetRepository.save(existingTweet);
+//                })
+//                .map(updateTweet -> new ResponseEntity<>(updateTweet, HttpStatus.OK))
+//                .defaultIfEmpty(new ResponseEntity<>(HttpStatus.NOT_FOUND));
+//    }
+//
+//    @DeleteMapping("/tweets/{id}")
+//    public Mono<ResponseEntity<Void>> deleteTweet(@PathVariable(value = "id") String tweetId) {
+//
+//        return tweetRepository.findById(tweetId)
+//                .flatMap(existingTweet ->
+//                        tweetRepository.delete(existingTweet)
+//                            .then(Mono.just(new ResponseEntity<Void>(HttpStatus.OK)))
+//                )
+//                .defaultIfEmpty(new ResponseEntity<>(HttpStatus.NOT_FOUND));
+//    }
+//
+//    // Tweets are Sent to the client as Server Sent Events
+//    @GetMapping(value = "/stream/tweets", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+//    public Flux<Tweet> streamAllTweets() {
+//        return tweetRepository.findAll();
+//    }
 
 
     /*
--- src/main/java/com/example/webfluxdemo/repository/TweetRepository.java
+++ src/main/java/com/example/webfluxdemo/repository/TweetRepository.java
@@ -1,8 +1,5 @@
 package com.example.webfluxdemo.repository;
 
-import com.example.webfluxdemo.model.Tweet;
-
-@EnableScan
-public interface TweetRepository extends ReactiveCrudRepository<Tweet, String> {
+public interface TweetRepository {
 
 }
